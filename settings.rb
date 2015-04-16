877d40275dfa21907b86080e5111a42a6a86a6c3
Sella Rafaeli <sella.rafaeli@gmail.com>
2015-04-16 15:40:23 +0300

    root url change

--- settings.rb
+++ settings.rb
@@ -11,4 +11,4 @@ set :algo_password,       ENV['PICKEEZ_ALGO_PASSWORD']
 # The following are exposed globally via $varname.
 $app_name   = 'pickeez'
 $prod       = settings.production? #env is set via RACK_ENV=production in prod 
-$root_url   = $prod ? 'http://pickeez.herokuapp.com' : 'http://localhost:8002'
+$root_url   = $prod ? 'http://pickeezmetadata.com' : 'http://localhost:8002'
